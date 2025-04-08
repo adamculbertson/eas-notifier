6cdac10a8eebc7ee0043e0b2c216098131cd13f3
Adam Culbertson <173273674+adamculbertson@users.noreply.github.com>
2025-04-08 03:30:40 -0400

    Use proper logging

--- scripts/dsame3/pipe.py
+++ scripts/dsame3/pipe.py
@@ -3,8 +3,25 @@ import subprocess
 import os
 import json
 import threading
+import logging
 
 import requests
+
+# Define the logger and set up the logging level
+logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s")
+
+# The level can be  whatever names setLevel() supports
+if "LOG_LEVEL" in os.environ:
+    level = os.environ['LOG_LEVEL']
+else:
+    # Default to INFO if not specified
+    level = logging.INFO
+
+logging.getLogger().setLevel(level)
+
+if "WEBHOOK_URL" not in os.environ:
+    logging.critical("No WEBHOOK_URL provided in the environment!")
+    sys.exit(1)
 
 webhook_url = os.environ['WEBHOOK_URL']
 
@@ -22,6 +39,11 @@ try:
 except KeyError:
     same = None
 
+if same is not None:
+    logging.debug(f"Number of SAME codes specified: {len(same)}")
+else:
+    logging.debug("No SAME codes specified")
+
 # Check if the authorization token exists in the environment and contains a valid "truthful" value (not empty quotes)
 try:
     webhook_authorization = os.environ['WEBHOOK_AUTHORIZATION']
@@ -29,6 +51,9 @@ try:
         webhook_authorization = None
 except KeyError:
     webhook_authorization = None
+
+if webhook_authorization is not None:
+    logging.debug("Using provided webhook authorization token")
 
 
 def parse_event(event: dict):
@@ -41,28 +66,30 @@ def parse_event(event: dict):
     try:
         r = requests.post(webhook_url, json=event, headers=headers)
     except Exception as e:
-        sys.stderr.write(f"Caught exception posting to webhook. {e}\n")
-        sys.stderr.flush()
+        logging.error(f"Caught exception posting to webhook. {e}")
         return
 
     if not r.ok:
-        sys.stderr.write(f"Error posting to webhook. Received status code {r.status_code}\n")
-        sys.stderr.write(f"Headers: {json.dumps(headers)}\n")
-        sys.stderr.flush()
+        logging.error(f"Error posting to webhook. Received status code {r.status_code}\nHeaders: {json.dumps(headers)}")
+        return
+
+    logging.info("Successfully posted to webhook!")
 
 
 if __name__ == "__main__":
-    sys.stdout.write("Ready to parse incoming alerts!\n")
-    sys.stdout.flush()
+    logging.info("Ready to parse incoming alerts!\n")
     for line in sys.stdin:
         # Skip the end message of an alert
         if "NNNN" in line:
+            logging.debug("Skipping NNNN line")
             continue
 
         if line.startswith("EAS:"):
             # Strip out the EAS: line from the output so that we can directly send it to dsame.py
-            line = line.replace("EAS: ", "")
+            line = line.replace("EAS: ", "").strip()
             # same_decode(line, "EN", same_watch=same, event_watch=None,
+
+            logging.debug(f"Alert received: {line}")
 
             # I would rather just call the function instead of using subprocess for this
             # However, given how much trouble it's been just getting some output and dealing with buffering
@@ -78,6 +105,8 @@ if __name__ == "__main__":
 
             params += ["--json", "-"]  # Have dsame output the JSON to stdout instead
 
+            logging.debug(f"dsame parameters: {params}")
+
             p = subprocess.run(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             if p.returncode == 0 and p.stdout:
                 try:
@@ -85,14 +114,12 @@ if __name__ == "__main__":
                 except json.JSONDecodeError:
                     # dsame sometimes outputs an empty string
                     # Output the line that causes the empty string for debugging
-                    sys.stderr.write(f"Error decoding JSON from dsame\n")
-                    sys.stderr.write(f"Output: {p.stdout}\n")
-                    sys.stderr.write(f"Line: {line}\n")
-                    sys.stderr.flush()
+                    logging.error(f"Error decoding JSON from dsame\nOutput: {p.stdout}\nLine: {line}")
                     continue
 
-                sys.stdout.write(js['MESSAGE'] + "\n")
-                sys.stdout.flush()
+                # Print out the fully parsed message
+                print(js['MESSAGE'])
+
                 js['line'] = line  # Add the raw data for the line to the payload
                 # Send the stdout of the process to the parse_event function in a new thread
                 threading.Thread(target=parse_event, args=(js,)).start()
@@ -100,10 +127,7 @@ if __name__ == "__main__":
             elif p.returncode == 0 and not p.stdout:
                 # Blank output can be caused by the use of the --same parameter
                 # When the alert does not match anything in the list of codes, then nothing is output
-                sys.stderr.write(f"dsame blank output: {line}\n")
-                sys.stderr.flush()
+                logging.debug(f"dsame blank output: {line}")
 
             else:
-                sys.stderr.write(f"Unexpected return value from dsame: {p.returncode}\n")
-                sys.stderr.write(f"Line: {line}\n")
-                sys.stderr.flush()
+                logging.error(f"Unexpected return value from dsame: {p.returncode}\nLine: {line}")
